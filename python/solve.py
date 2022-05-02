f883af4de323ff9a287cfa4e7e08ef7de2fe05d5
angelica-w-berkeley <angelica_w@berkeley.edu>
2022-05-02 15:26:54 -0700

    Update solve.py
    greedy

--- python/solve.py
+++ python/solve.py
@@ -11,6 +11,7 @@ from typing import Callable, Dict
 
 from instance import Instance
 from solution import Solution
+from point import Point
 from file_wrappers import StdinFileWrapper, StdoutFileWrapper
 
 
@@ -20,9 +21,63 @@ def solve_naive(instance: Instance) -> Solution:
         towers=instance.cities,
     )
 
+def cover_matrix(instance):
+    # 0 = not covered; 1 = covered/ not city
+    # init matrix to all 1s
+    side = instance.grid_side_length
+    mat = [[1 for x in range(side)] for y in range(side)]
+    # set coords of cities to 0
+    cities = instance.cities
+    for c in cities:
+        x = c.x 
+        y = c.y
+        mat[x][y] = 0
+    return mat 
+
+def will_cover(cities, mat, tow, r):
+    cov_cities = []
+    for c in cities:
+        if mat[c.x][c.y] == 0:
+            dis = Point.distance_obj(c, tow)
+            if (dis <= r):
+                cov_cities.append(c)
+    return cov_cities
+                
+def update_matrix(mat, cities):
+    # uptdate cities that will be covered to 1 in matrix
+    for c in cities:
+        mat[c.x][c.y] = 1
+    return mat
+
+def solve_greedy(instance: Instance) -> Solution:
+    # create matrix for whether city is covered or not
+    mat = cover_matrix(instance)
+    side = instance.grid_side_length
+    towers = []
+    uncovered = len(instance.cities)
+    # loop through matrix, finding tower placement that covers most uncovered cities
+    # continue until all cities are covered
+    while uncovered > 0:
+        # towers.append(Point(100, 100))
+        max_cities = []
+        max_tower = Point(0, 0)
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                tower = Point(i, j)
+                covered = will_cover(instance.cities, mat, tower, instance.coverage_radius)
+                if len(covered) > len(max_cities):
+                    max_cities = covered
+                    max_tower = tower
+
+        mat = update_matrix(mat, max_cities)
+        towers.append(max_tower)
+        uncovered -= len(max_cities)
+    return Solution(instance=instance, towers=towers)
+
 
 SOLVERS: Dict[str, Callable[[Instance], Solution]] = {
-    "naive": solve_naive
+    "naive": solve_naive,
+    "greedy": solve_greedy
 }
 
 
